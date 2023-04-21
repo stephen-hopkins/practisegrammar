0b8bec5e686eb9ebf7e3112a09ade94754857c10
Stephen Hopkins <github@stephenhopkins.dev>
2023-04-21 14:19:45 +0300

    test: fix test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,8 +1,15 @@
 import Home from "@/src/pages";
-import {render} from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { render } from "@testing-library/react";
+import React, { ReactNode } from "react";
 
-describe('Index', () => {
-  it('renders', () => {
-    render(<Home />);
-  })
-})
+const queryClient = new QueryClient();
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe("Index", () => {
+  it("renders", () => {
+    render(wrapper({ children: <Home /> }));
+  });
+});
