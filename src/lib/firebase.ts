3ce2c2715720b601c762338754b26a153f90d866
Stephen Hopkins <github@stephenhopkins.dev>
2023-04-21 14:44:17 +0300

    fix: firebase build

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,7 @@
-import {initializeApp} from "firebase/app";
-import {getAnalytics} from "firebase/analytics";
-import {Concept, Language} from "@/src/models/constants";
-import {
-  addDoc,
-  collection, CollectionReference,
-  doc,
-  FirestoreDataConverter, getDocs,
-  getFirestore, query,
-  QueryDocumentSnapshot, SnapshotOptions, where
-} from "@firebase/firestore";
-import {Sentence, SentenceInfo} from "@/src/models/translation";
-import {FirebaseFirestore} from "@firebase/firestore-types";
+import { initializeApp } from "firebase/app";
+import { Concept, Language } from "@/src/models/constants";
+import { addDoc, collection, CollectionReference, getDocs, getFirestore, query, where } from "@firebase/firestore";
+import { Sentence, SentenceInfo } from "@/src/models/translation";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXVeg1hzBtWVW2FK9_cz7r7gygHIH8E2k",
@@ -19,38 +10,36 @@ const firebaseConfig = {
   storageBucket: "practisegrammar.appspot.com",
   messagingSenderId: "666711626320",
   appId: "1:666711626320:web:410a88534639301d4b7755",
-  measurementId: "G-YCLPJ0Z093"
+  measurementId: "G-YCLPJ0Z093",
 };
 
 function getDb() {
-
   // const analytics = getAnalytics(app);
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
 
   return {
-    sentences: collection(db, "sentences") as CollectionReference<SentenceInfo>
+    sentences: collection(db, "sentences") as CollectionReference<SentenceInfo>,
   };
-
 }
 
 export async function getSentence(language: Language, concept: Concept, word: string) {
-  const {sentences} = getDb();
-  const q = query(sentences,
+  const { sentences } = getDb();
+  const q = query(
+    sentences,
     where("language", "==", language),
     where("concept", "==", concept),
-    where("word", "==", word));
+    where("word", "==", word)
+  );
   const querySnapshot = await getDocs(q);
   if (!querySnapshot.empty) {
     return querySnapshot.docs[0].data();
   }
 }
 
-
 export async function addSentence(sentence: Sentence) {
-  const {translations, ...metaData} = sentence;
-  const {sentences} = getDb();
-  const doc = await addDoc(sentences, metaData, "translations", translations);
-  await addDoc(collection(sentences, doc.id, "translations"), translations);
+  const { translations, ...metaData } = sentence;
+  const { sentences } = getDb();
+  //const doc = await addDoc(sentences, metaData, "translations", translations);
+  //await addDoc(collection(sentences, doc.id, "translations"), translations);
 }
-
